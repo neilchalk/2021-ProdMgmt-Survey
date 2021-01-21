9531c743ad7f9025690251f85e30fd4ab0d24ab2
Neil Chalk <neil.chalk@gmail.com>
2021-01-21 15:01:15 +0000

    Add in an Orchestration script

---
+++ code/script-runner.R
@@ -0,0 +1,4 @@
+source('~/R/2021-ProdMgmt-Survey/code/survey-pre-process.R', echo=TRUE)
+source('~/R/2021-ProdMgmt-Survey/code/survey-modelling.R', echo=TRUE)
+source('~/R/2021-ProdMgmt-Survey/code/survey-descriptive.R', echo=TRUE)
+source('~/R/2021-ProdMgmt-Survey/code/survey-explore.R', echo=TRUE)

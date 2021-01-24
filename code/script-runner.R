dd5d7b261a6b87d707dc5c7191b22b4c8a6166f1
Neil Chalk <neil.chalk@gmail.com>
2021-01-24 12:17:58 +0000

    add some explaination to script runner
    also only output script output and not the commands/comments

--- code/script-runner.R
+++ code/script-runner.R
@@ -1,4 +1,8 @@
-source('~/R/2021-ProdMgmt-Survey/code/survey-pre-process.R', echo=TRUE)
-source('~/R/2021-ProdMgmt-Survey/code/survey-modelling.R', echo=TRUE)
-source('~/R/2021-ProdMgmt-Survey/code/survey-descriptive.R', echo=TRUE)
-source('~/R/2021-ProdMgmt-Survey/code/survey-explore.R', echo=TRUE)
+# Load in raw data from google forms and do initial data tidying
+source('~/R/2021-ProdMgmt-Survey/code/survey-pre-process.R', echo=FALSE, print.eval = TRUE)
+# fit the data to the DEEP v1.1 roadmap maturity model and IPSMA v1.3 SPM framwork (refs in source)
+source('~/R/2021-ProdMgmt-Survey/code/survey-modelling.R', echo=FALSE, print.eval = TRUE)
+# produce some descriptive visualisations and RMarkdown output report
+source('~/R/2021-ProdMgmt-Survey/code/survey-descriptive.R', echo=FALSE, print.eval = TRUE)
+# look for relationships and produce latest stats on previously identified relationships
+source('~/R/2021-ProdMgmt-Survey/code/survey-explore.R', echo=FALSE, print.eval = TRUE)
